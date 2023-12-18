9d3d8a0d062ea04a70385dd0b9c6e54fbbc06c6b
RiskevO3 <91577090+RiskevO3@users.noreply.github.com>
2023-12-18 13:20:09 +0700

    reinit gitignore

--- resources/views/components/login-register.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['image' => 'default'])
-<div class="grid grid-cols-2 h-screen">
-    <div class="overflow-hidden rounded-r-[100px] bg-red-700 h-screen">
-        <img src="{{ asset('assets/images/lomba-'.$image.'-bg-login.jpeg') }}" alt=""
-            class="object-cover object-center" loading="lazy">
-    </div>
-    {{ $slot }}
-</div>
--- resources/views/components/logo.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="navbar-item-logo">
-<img
-    src="{{ asset('assets/images/logo-carilomba.png') }}"
-    alt=""
-    srcset=""
-    class="bg-cover bg-center"
-/>
-</div>
