844979d332a99ae38336b43635246592be417377
RiskevO3 <mrmultazam@student.telkomuniversity.ac.id>
2023-12-29 09:03:30 +0700

    Update color in LombaResource and StatsOverview

--- app/Filament/Penyelenggara/Widgets/StatsOverview.php
+++ app/Filament/Penyelenggara/Widgets/StatsOverview.php
@@ -5,34 +5,34 @@ namespace App\Filament\Penyelenggara\Widgets;
 use App\Models\Lomba;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Auth;
 
 class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
-        function getTotalPeserta($active = false){
-            $total = 0;
-            if($active){
-                foreach (Lomba::where('penyelenggara_id',auth()->user()->penyelenggara->id)->where('is_active',true)->with('pendaftar')->get() as $lomba) {
-                    $total += $lomba->pendaftar->count();
-                }
-                return $total;
-            }else{
-                foreach (Lomba::where('penyelenggara_id',auth()->user()->penyelenggara->id)->with('pendaftar')->get() as $lomba) {
-                    $total += $lomba->pendaftar->count();
-                }
+        $totalLomba = 0;
+        $totalLombaAktif = 0;
+        $totalPendaftar = 0;
+        $totalPendaftarAktif = 0;
+        $allLomba = Lomba::where('penyelenggara_id',Auth::user()->penyelenggara->id)->with('pendaftar')->get();
+        foreach ($allLomba as $lomba) {
+            $totalPendaftar += $lomba->pendaftar->count();
+            $totalLomba++;
+            if ($lomba->is_active) {
+                $totalLombaAktif++;
+                $totalPendaftarAktif += $lomba->pendaftar->count();
             }
-            return $total;
         }
         return [
-            Stat::make('Total Lomba',Lomba::where('penyelenggara_id',auth()->user()->penyelenggara->id)->count())
+            Stat::make('Total Lomba',$totalLomba)
             ->icon('icon-medal'),
-            Stat::make('Total Lomba Aktif',Lomba::where('penyelenggara_id',auth()->user()->penyelenggara->id)->where('is_active',true)->count())
+            Stat::make('Total Lomba Aktif',$totalLombaAktif)
             ->icon('icon-check'),
-            Stat::make('Total Peserta',getTotalPeserta())
+            Stat::make('Total Peserta',$totalPendaftar)
             ->icon('icon-user-circle'),
             // get total peserta aktif 
-            Stat::make('Total Peserta Aktif',getTotalPeserta(true))
+            Stat::make('Total Peserta Aktif',$totalPendaftarAktif)
             ->icon('icon-user-active')
         ];
     }
